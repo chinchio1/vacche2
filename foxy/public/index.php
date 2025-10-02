b819175e8db82c740cada16d30b5ca07ee940613
Didiliuz <Fboukourri2@gmail.com>
2025-10-02 22:52:37 +0200

    creazione base progetto e file readme.md

---
+++ foxy/public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Benvenuto nel progetto PHP</h1>
+    <p>Scegli come vuoi inserire i dati:</p>
+    <ul>
+        <li><a href="insert.php">Inserimento Manuale</a></li>
+        <li><a href="upload_excel.php">Caricamento da Excel</a></li>
+    </ul>
+</body>
+</html>
